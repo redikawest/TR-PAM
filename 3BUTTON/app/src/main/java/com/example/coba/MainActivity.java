e9cea72d0f0432feda690b6b31caa0eb74507c6c
Evan Bagus Kristianto <49176563+EvanBagusKristianto@users.noreply.github.com>
2020-03-30 14:57:06 +0700

    3Tombol

---
+++ 3BUTTON/app/src/main/java/com/example/coba/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.coba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main4Activity.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
